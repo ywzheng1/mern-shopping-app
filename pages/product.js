10c4d2746cbb43a0b2ec8f19508e47b2a060fc60
Yunwen Zheng <ynn.zheng@gmail.com>
2020-06-25 22:18:27 -0400

    get product by id

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,15 @@
-function Product() {
+import axios from 'axios'
+
+function Product({ product }) {
+	console.log(product)
   return <>product</>;
+}
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+	const url = "http://localhost:3000/api/product";
+    const payload = { params: { _id } };
+	const response = await axios.get(url, payload);
+	return { product : response.data }
 }
 
 export default Product;
