c8f8a11511a43bd80291b6bef16c1c7afbbcc13e
Yunwen Zheng <ynn.zheng@gmail.com>
2020-06-25 23:12:50 -0400

    added delete method on backend, and delete functions on front-end allow user to delete product

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,20 @@
-import { Header, Button } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Header, Button, Modal } from 'semantic-ui-react';
+import axios from 'axios';
+import baseUrl from '../../utils/baseUrl';
+import { useRouter } from 'next/router'
 
-function ProductAttributes({ description }) {
+function ProductAttributes({ description, _id }) {
+	const [modal, setModal] = useState(false);
+	const router = useRouter();
+
+	async function handleDelete() {
+		const url = `${baseUrl}/api/product`;
+		const payload = { params: { _id } }
+		await axios.delete(url, payload)
+		router.push('/');
+	}
+
 	return (
 		<>
 			<Header as='h3'>About this product</Header>
@@ -9,7 +23,27 @@ function ProductAttributes({ description }) {
 				icon='trash alternate outline'
 				color='red'
 				content='Delete Product'
+				onClick={() => setModal(true)}
 			/>
+			<Modal open={modal} dimmer='blurring'>
+				<Modal.Header>Confirm Delete</Modal.Header>
+				<Modal.Content>
+					<p>Are you sure you want to delete this product?</p>
+				</Modal.Content>
+				<Modal.Actions>
+					<Button 
+						content="Cancel" 
+						onClick={() => setModal(false)}
+					/>
+					<Button 
+						negative
+						icon='trash'
+						labelPosition='right'
+						content='Delete' 
+						onClick={handleDelete}
+					/>
+				</Modal.Actions>
+			</Modal>
 		</>
 	);
 }
