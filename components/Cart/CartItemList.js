20a9b542f70afa5e099de4d533e9f9e7b979ad02
Yunwen Zheng <ynn.zheng@gmail.com>
2020-06-28 16:41:12 -0400

    styled cart page

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,27 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react';
+
 function CartItemList() {
-  return <>CartItemList</>;
+
+  const user = false;
+
+  return (
+    <Segment secondary color='teal' inverted textAlign='center' placeholder>
+      <Header icon>
+        <Icon name='shopping basket'/>
+        No products in your cart. 
+      </Header>
+      <div>
+        {user ? (
+          <Button color='orange'>View Products</Button>
+        ) : (
+          <Button color='blue'>
+            Login to Add Products
+          </Button>
+        )
+        }
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
