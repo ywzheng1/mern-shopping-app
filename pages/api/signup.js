728b5ca4d019c0be4d6fb2c6b2f002bac29a4cd2
Yunwen Zheng <ynn.zheng@gmail.com>
2020-06-28 20:25:24 -0400

    create user cart upon signup

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,6 @@
 import connectDb from '../../utils/connectDb';
 import User from '../../models/User';
+import Cart from '../../models/Cart';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import isEmail from 'validator/lib/isEmail';
@@ -34,11 +35,13 @@ export default async (req, res) => {
             password: hash
         }).save()
         console.log({newUser})
-        // 5) create token for the new user
+        // 5) create cart for new user
+        await new Cart({ user: newUser._id }).save()
+        // 6) create token for the new user
         const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {
                             expiresIn: '7d'
                         })
-        // 6) send back token
+        // 7) send back token
         res.status(201).json(token)
     } catch(error) {
         console.error(error)
