30578a802b556882acebcb98b686e9fd9cfdef8b
Yunwen Zheng <ynn.zheng@gmail.com>
2020-07-26 18:32:14 -0400

    add pagination for product list

--- pages/api/products.js
+++ pages/api/products.js
@@ -5,6 +5,19 @@ import connectDb from '../../utils/connectDb';
 connectDb();
 
 export default async (req, res) => {
-    const products = await Product.find()
-    res.status(200).json(products)
+    const { page, size } = req.query
+    //convert querystring values to numbers
+    const pageNum = Number(page)
+    const pageSize = Number(size)
+    let products = [];
+    const totalDocs = await Product.countDocuments()
+    const totalPages = Math.ceil(totalDocs / pageSize);
+    if (pageNum === 1) {
+        products = await Product.find().limit(pageSize)
+    } else {
+        const skips = pageSize * (pageNum - 1)
+        products = await Product.find().skip(skips).limit(pageSize)
+    }
+    // const products = await Product.find()
+    res.status(200).json({products, totalPages})
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 import React, { useEffect } from 'react';
 import axios from 'axios'
 import ProductList from '../components/Index/ProductList';
+import ProductPagination from '../components/Index/ProductPagination';
 import baseUrl from '../utils/baseUrl';
 
 
-function Home({ products }) {
-	return <ProductList products={products}/>;
+function Home({ products, totalPages }) {
+	return (
+		<>
+			<ProductList products={products}/>
+			<ProductPagination totalPages={totalPages} />
+		</>
+	);
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+	const page = ctx.query.page ? ctx.query.page : "1";
+	const size = 9;
 	// fetch data on server
 	const url = `${baseUrl}/api/products`
-	const res = await axios.get(url)
+	const payload = { params: { page, size }}
+	const res = await axios.get(url, payload)
 
 	// return response data as an object
-	return { products: res.data }
+	return res.data;
 	// note: this object will be merged with existing props
 }
 
